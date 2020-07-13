17907244f434a0d0899071c93d4ed095730d1ef7
shefer <1429907181@qq.com>
2020-07-13 14:46:26 +0800

    Update ArraySort.js

--- ArraySort.js
+++ ArraySort.js
@@ -5,9 +5,13 @@ Array.prototype.mySort = function() {
     for(var i = 0; i < len; i++) {
         for(var j = 0; j < len - i - 1; j++) {
             var func = args[0];
-            if(func && typeof func == 'function') {
-                if(func.call(args, arr[j], arr[j+1]) > 0){
-                  [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
+            if(func !== undefined) {
+                if(typeof func == 'function') {
+                    if(func.call(args, arr[j], arr[j+1]) > 0){
+                      [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
+                    }
+                } else {
+                    throw new Error('The comparison function must be either a function or undefined');
                 }
             } else {
                 var str1 = arr[j].toString()
