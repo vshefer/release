453669b75d9ad0cad09f0b5bd5c5295766f7872f
shefer <1429907181@qq.com>
2020-07-13 14:14:00 +0800

    Update ArraySort.js

--- ArraySort.js
+++ ArraySort.js
@@ -1,12 +1,9 @@
 Array.prototype.mySort = function() {
-    console.log('arguments:', arguments);
-    console.log('this:', this);
     var args = Array.from(arguments);
-    console.log('args:', args);
     var arr = this;
     var len = arr.length;
     for(var i = 0; i < len; i++) {
-        for(var j = 0; j < len - i; j++) {
+        for(var j = 0; j < len - i - 1; j++) {
             if(args[0] && typeof args[0] == 'function') {
                 if(args[0](arr[j], arr[j+1]) > 0){
                   [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
