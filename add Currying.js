1c4acd112e7fe58d9b6d8ce35b5590e33897de6a
shefer <1429907181@qq.com>
2020-08-06 17:43:36 +0800

    Update add Currying.js

--- add Currying.js
+++ add Currying.js
@@ -1,10 +1,8 @@
 function add() {
     var args = [...arguments]
-
-    var _addr = function() {
+    return function() {
         args.push(...arguments)
-        arguments.callee.toString = function() {return args.reduce((a,b) => a + b)}
+        arguments.callee.toString = () => {return args.reduce((a, b) => a + b)}
         return arguments.callee
-    }
-    return _addr
+    }()
 }
