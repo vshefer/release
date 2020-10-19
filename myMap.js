b90a6cbf1799886f62f52b917ffdf0573f659114
shefer <1429907181@qq.com>
2020-10-19 14:46:54 +0800

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -2,7 +2,7 @@ function myMap() {
     var args = Array.from(arguments);
 //     var funcBase = arguments.callee;
     var func = args[0];
-    var funcArg = args[1];
+    var funcThis = args[1];
     var theOne = this;
 
     var len = this.length;
@@ -10,7 +10,7 @@ function myMap() {
     for(var i = 0; i < len; i++) {
         var currentValue = theOne[i];
         var index = i;
-        var callBack = func.call(funcArg, currentValue, index, theOne);
+        var callBack = func.call(funcThis, currentValue, index, theOne);
         callBackArr.push(callBack);
     }
 
