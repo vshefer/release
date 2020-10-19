933c92b3cf684b84489e19fe1611bbc5e880a8fd
shefer <1429907181@qq.com>
2020-10-19 14:26:35 +0800

    Create myMap.js

---
+++ myMap.js
@@ -0,0 +1,18 @@
+function myMap() {
+    var args = Array.from(arguments);
+//     var funcBase = arguments.callee;
+    var func = args[0];
+    var funcArg = args[1] || this;
+    var theOne = this;
+
+    var len = this.length;
+    var callBackArr = []
+    for(var i = 0; i < len; i++) {
+        var currentValue = theOne[i];
+        var index = i;
+        var callBack = func.call(funcArg, currentValue, index, theOne);
+        callBackArr.push(callBack);
+    }
+
+    return callBackArr;
+}
